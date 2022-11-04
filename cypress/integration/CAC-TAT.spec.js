86826e09084a56084ac83039da951a00f98ace70
Flávio Leão da Gama <38006748+leaogama@users.noreply.github.com>
2022-11-04 09:26:54 -0300

    ex03

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -12,21 +12,31 @@ describe('Central de Atendimento ao Cliente TAT', function () {
 
     it('Preencher campos obrigatórios e enviar formulário', () => {
         let z = "Gostaria de receber uma copia do meu contrato"
-        cy.get('input#firstName').should('be.visible').type('Flavio').should('have.value','Flavio')
-        cy.get('input#lastName').should('be.visible').type('Leão').should('have.value','Leão')
-        cy.get('input#email').should('be.visible').type('leaogama@gmail.com').should('have.value','leaogama@gmail.com')
-        cy.get('#open-text-area').should('be.visible').type(z,{delay:10}).should('have.value', z)
+        cy.get('input#firstName').should('be.visible').type('Flavio').should('have.value', 'Flavio')
+        cy.get('input#lastName').should('be.visible').type('Leão').should('have.value', 'Leão')
+        cy.get('input#email').should('be.visible').type('leaogama@gmail.com').should('have.value', 'leaogama@gmail.com')
+        cy.get('#open-text-area').should('be.visible').type(z, { delay: 0 }).should('have.value', z)
         cy.get('.button').should('be.visible').click()
         cy.get('.success').should('be.visible').should('contain.text', 'sucesso')
     })
-it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () =>{
-    let z = "Gostaria de receber uma copia do meu contrato"
-        cy.get('input#firstName').should('be.visible').type('Flavio').should('have.value','Flavio')
-        cy.get('input#lastName').should('be.visible').type('Leão').should('have.value','Leão')
-        cy.get('input#email').should('be.visible').type('leaogama@gmailcom').should('have.value','leaogama@gmailcom')
-        cy.get('#open-text-area').should('be.visible').type(z,{delay:10}).should('have.value', z)
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+        let z = "Gostaria de receber uma copia do meu contrato"
+        cy.get('input#firstName').should('be.visible').type('Flavio').should('have.value', 'Flavio')
+        cy.get('input#lastName').should('be.visible').type('Leão').should('have.value', 'Leão')
+        cy.get('input#email').should('be.visible').type('leaogama@gmailcom').should('have.value', 'leaogama@gmailcom')
+        cy.get('#open-text-area').should('be.visible').type(z, { delay: 0 }).should('have.value', z)
         cy.get('.button').should('be.visible').click()
         cy.get('.error').should('be.visible').should('contain.text', 'campos')
-})
+    })
+
+    it('Erro- ele aceita numeros e "." no campo Telefone', () => {
+        // let z = "Gostaria de receber uma copia do meu contrato"
+        // cy.get('input#firstName').should('be.visible').type('Flavio').should('have.value', 'Flavio')
+        // cy.get('input#lastName').should('be.visible').type('Leão').should('have.value', 'Leão')
+        // cy.get('input#email').should('be.visible').type('leaogama@gmail.com').should('have.value', 'leaogama@gmail.com')
+        // cy.get('#open-text-area').should('be.visible').type(z, { delay: 0 }).should('have.value', z)
+        // cy.get('.button').should('be.visible').click()
+        cy.get('#phone').should('be.visible').type('abcdfgh/@#(51)9.9').should("have.value", '519.9')
+    })
 
 })
