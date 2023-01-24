1257a9879bdfa31d1b2e1ac5a9946700a9663994
Flávio Leão da Gama <38006748+leaogama@users.noreply.github.com>
2023-01-24 18:03:22 -0300

    aula 07

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,5 +1,9 @@
 /// <reference types="Cypress" />
 
+
+
+
+
 describe('Central de Atendimento ao Cliente TAT', function () {
 
     beforeEach(() => {
@@ -44,7 +48,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('input#firstName').should('be.visible').type('Flavio').should('have.value', 'Flavio')
         cy.get('input#lastName').should('be.visible').type('Leão').should('have.value', 'Leão')
         cy.get('input#email').should('be.visible').type('leaogama@gmail.com').should('have.value', 'leaogama@gmail.com')
-        cy.get('#phone-checkbox').click()
+        cy.get('#phone-checkbox').check()
         cy.get('#phone').should('be.visible').type('abcdfgh/@#').should("have.value", '')
         cy.get('#open-text-area').should('be.visible').type(z, { delay: 1 }).should('have.value', z)
         cy.contains('button', 'enviar', { matchCase: false }).should('be.visible').click()
@@ -92,7 +96,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('#product').should('have.value', 'blog')
     });
 
-    it('seleciona toodos os elementos do tipo radio', () => {
+    it('ct11-seleciona toodos os elementos do tipo radio', () => {
         // cy.get('[type="radio"]').check()
         //  cy.get('[type="radio"]').should('contain.value', 'feedback')
 
@@ -100,19 +104,87 @@ describe('Central de Atendimento ao Cliente TAT', function () {
         cy.get('[type="checkbox"]').check('email')
     })
 
-    it('Marca o tipo de atendimento "feedback"', function () {
-        cy.get('input[type="radio"][value="feedback"').check().should('have.value','feedback')
+    it('ct12-Marca o tipo de atendimento "feedback"', function () {
+        cy.get('input[type="radio"][value="feedback"').check().should('have.value', 'feedback')
 
     })
 
-    it.only('Marca cada tipo de atendimento', ()=> {
+    it('ct13-Marca cada tipo de atendimento', () => {
         cy.get('input[type="radio"]')
-        .should('have.length', 3)
-        .each(function($radio){
-            cy.wrap($radio).check().should('be.checked')
-        })
+            .should('have.length', 3)
+            .each(function ($radio) {
+                cy.wrap($radio).check()
+                cy.wrap($radio).should('be.checked')
+            })
 
     })
+
+
+    it('ct14- Marca ambos os checkbox e depois desmarca a ultima', () => {
+        cy.get('input[type="checkbox"]').each(function ($cb) {
+            cy.wrap($cb).check()
+            cy.wrap($cb).should('be.checked')
+            
+        }).last().uncheck()
+    });
+
+    it('ct14b- Marca ambos os checkbox e depois desmarca a ultima', () => {
+        cy.get('input[type="checkbox"]')
+        .check()
+        .should('be.checked')
+        .last()
+        .uncheck()
+        .should('not.be.checked')
+    });
+    it('ct15- desarca ambos os checkbox ', () => {
+        cy.get('input[type="checkbox"]').each(function ($cb) {
+            cy.wrap($cb).uncheck()
+            cy.wrap($cb).should('not.be.checked')
+            
+        }).last().uncheck()
+    });
+
+    it('ct16- outra forma de marcar all checkbox', () => {
+        cy.get('input[type="checkbox"]').as('contato').check()
+        cy.get('@contato').each(ch =>{
+            expect(ch[0].checked).to.equal(true)
+        })
+    })
+
+    it('ct17- seleciona um arquivo da pasta fixture', () => {
+        cy.get('input[type="file"]').selectFile('cypress/fixtures/example.json')
+        .then(input => {
+            console.log(input)
+            expect(input[0].files[0].name).to.equal('example.json')
+        })
+    });
+
+    it('ct18- seleciona um arquivo da pasta fixture simulando drag-and-drop', () => {
+        cy.get('input[type="file"]')
+        .selectFile('cypress/fixtures/example.json', {action:'drag-drop'})
+        .then(input => {
+            console.log(input)
+            expect(input[0].files[0].name).to.equal('example.json')
+        })
+    });
+
+    it('ct19-seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', () => {
+        cy.fixture("example.json", { encoding:null }).as('exampleFile')
+        cy.get('input[type="file"]')
+        .selectFile('@exampleFile')
+        .then($input => {
+            console.log($input)
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+    });
+
+    it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', () => {
+        cy.get('#privacy a')
+    });
+    // it.only('ct15 - desmarca último checkbox', () => {
+    //     cy.get('input[type="checkbox"]').last()
+    //     })
+    
 
     // cy.get('div').contains('capital sentence', { matchCase: false })
     // cy.contains('Delete User').click().contains('Yes, Delete!').click()
