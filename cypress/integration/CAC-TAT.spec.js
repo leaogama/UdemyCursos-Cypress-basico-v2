ef29f8350318913a61a50ba75a126473c43a52bb
Flávio Leão da Gama <38006748+leaogama@users.noreply.github.com>
2023-01-24 20:37:46 -0300

    fim por hoje

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -187,7 +187,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .click()
         cy.contains('Talking About Testing').should('be.visible')
     });
-    it.only('ct22-testa a página da política de privavidade de forma independente', () => {
+    it('ct22-testa a página da política de privavidade de forma independente', () => {
         cy.visit('./src/privacy.html')
         cy.contains('Talking About Testing').should('be.visible')
     });
